b4c241b30708c94ac550fe2fbd71ba2c0375b74d
Kaiwen Xu <kevin@kevxu.net>
2021-03-26 15:48:23 -0700

    enable / tear down prometheus server

--- clusterloader2/run-local.sh
+++ clusterloader2/run-local.sh
@@ -12,4 +12,6 @@ ETCD_KEY=/etc/kubernetes/pki/etcd/server.key \
     --masterip=k8s-cp-01.intern.kevxu.net,k8s-cp-02.intern.kevxu.net,k8s-cp-03.intern.kevxu.net \
     --etcdip=k8s-etcd-01.intern.kevxu.net,k8s-etcd-02.intern.kevxu.net,k8s-etcd-03.intern.kevxu.net \
     --etcd-certificate=/etc/kubernetes/pki/apiserver-etcd-client.crt \
-    --etcd-key=/etc/kubernetes/pki/apiserver-etcd-client.key
+    --etcd-key=/etc/kubernetes/pki/apiserver-etcd-client.key \
+    --enable-prometheus-server \
+    --tear-down-prometheus-server
