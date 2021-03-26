0780b27b63a6e9992f0741429e2c72bc88fa25d0
Kaiwen Xu <kevin@kevxu.net>
2021-03-26 12:02:26 -0700

    Use go run and use FQDN for master ips

--- clusterloader2/run-local.sh
+++ clusterloader2/run-local.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-KUBE_SSH_USER=root ./clusterloader --kubeconfig=$HOME/.kube/config --testconfig=testing/load/config.yaml --provider=local --nodes=3 --report-dir=loadreport --masterip=k8s-cp-01,k8s-cp-02,k8s-cp-03 --etcd-certificate=/etc/kubernetes/pki/apiserver-etcd-client.crt --etcd-key=/etc/kubernetes/pki/apiserver-etcd-client.ke
+KUBE_SSH_USER=root \
+    go run cmd/clusterloader.go \
+    --kubeconfig=$HOME/.kube/config \
+    --testconfig=testing/load/config.yaml \
+    --provider=local \
+    --nodes=3 \
+    --report-dir=loadreport \
+    --masterip=k8s-cp-01.intern.kevxu.net,k8s-cp-02.intern.kevxu.net,k8s-cp-03.intern.kevxu.net \
+    --etcd-certificate=/etc/kubernetes/pki/apiserver-etcd-client.crt \
+    --etcd-key=/etc/kubernetes/pki/apiserver-etcd-client.key
