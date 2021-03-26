ce4c8bd6325dfc15293e1f2fd1661d6583233a0f
Kaiwen Xu <kevin@kevxu.net>
2021-03-26 12:27:21 -0700

    Use different etcd endpoints.

--- clusterloader2/run-local.sh
+++ clusterloader2/run-local.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 KUBE_SSH_USER=root \
+ETCD_CERTIFICATE=/etc/kubernetes/pki/etcd/server.crt \
+ETCD_KEY=/etc/kubernetes/pki/etcd/server.key \
     go run cmd/clusterloader.go \
     --kubeconfig=$HOME/.kube/config \
     --testconfig=testing/load/config.yaml \
@@ -8,5 +10,6 @@ KUBE_SSH_USER=root \
     --nodes=3 \
     --report-dir=loadreport \
     --masterip=k8s-cp-01.intern.kevxu.net,k8s-cp-02.intern.kevxu.net,k8s-cp-03.intern.kevxu.net \
+    --etcdip=k8s-etcd-01.intern.kevxu.net,k8s-etcd-02.intern.kevxu.net,k8s-etcd-03.intern.kevxu.net \
     --etcd-certificate=/etc/kubernetes/pki/apiserver-etcd-client.crt \
     --etcd-key=/etc/kubernetes/pki/apiserver-etcd-client.key
