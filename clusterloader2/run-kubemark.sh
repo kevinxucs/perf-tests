ab95d62a86ffd5801df891cc1147c6f0ebfd7ea3
Kevin Xu <kevinxu@uber.com>
2021-03-31 15:46:05 -0700

    create test config map

--- clusterloader2/run-kubemark.sh
+++ clusterloader2/run-kubemark.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+ROOT=$(dirname ${BASH_SOURCE[0]})
+
+declare -A TEST_CONFIG
+TESTS_CONFIG[load]="${ROOT}/testing/load/config.yaml"
+TESTS_CONFIG[density]="${ROOT}/testing/density/config.yaml"
+
+test="$1"
+if [[ -z "$tests" ]]; then
+    echo "missing test"
+    exit 0
+fi
+
 KUBE_SSH_USER=root \
 ETCD_CERTIFICATE=/etc/kubernetes/pki/etcd/server.crt \
 ETCD_KEY=/etc/kubernetes/pki/etcd/server.key \
     go run cmd/clusterloader.go \
     -v=4 \
     --kubeconfig=$HOME/.kube/km-config \
-    --testconfig=testing/load/config.yaml \
+    --testconfig=${TEST_CONFIG[$test]}} \
     --provider=kubemark \
     --nodes=60 \
-    --report-dir=report_load_kubemark-cluster \
+    --report-dir=report_$test_kubemark-cluster \
     --masterip=k8s-km-01.intern.kevxu.net \
     --etcd-certificate=/etc/kubernetes/pki/apiserver-etcd-client.crt \
     --etcd-key=/etc/kubernetes/pki/apiserver-etcd-client.key \
--- clusterloader2/run-local.sh
+++ clusterloader2/run-local.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+ROOT=$(dirname ${BASH_SOURCE[0]})
+
+declare -A TEST_CONFIG
+TESTS_CONFIG[load]="${ROOT}/testing/load/config.yaml"
+TESTS_CONFIG[density]="${ROOT}/testing/density/config.yaml"
+
+test="$1"
+if [[ -z "$tests" ]]; then
+    echo "missing test"
+    exit 0
+fi
+
 KUBE_SSH_USER=root \
 ETCD_CERTIFICATE=/etc/kubernetes/pki/etcd/server.crt \
 ETCD_KEY=/etc/kubernetes/pki/etcd/server.key \
     go run cmd/clusterloader.go \
     -v=4 \
     --kubeconfig=$HOME/.kube/config \
-    --testconfig=testing/load/config.yaml \
+    --testconfig=${TEST_CONFIG[$test]} \
     --provider=local \
     --nodes=3 \
-    --report-dir=report_load_local-cluster \
+    --report-dir=report_$test_local-cluster \
     --masterip=k8s-cp-01.intern.kevxu.net,k8s-cp-02.intern.kevxu.net,k8s-cp-03.intern.kevxu.net \
     --etcdip=k8s-etcd-01.intern.kevxu.net,k8s-etcd-02.intern.kevxu.net,k8s-etcd-03.intern.kevxu.net \
     --etcd-certificate=/etc/kubernetes/pki/apiserver-etcd-client.crt \
